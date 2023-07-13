8e4dd51a6af9bca969e1f6a37e8d8ac03b0d1a58
Otlotleng24 <phenyomodise.pm@gmail.com>
2023-07-13 14:16:27 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - printing a line that uses a puts function
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
