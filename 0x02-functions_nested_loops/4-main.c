f7dd5cc3ea36f14f3c460f0f79e95af1ffe2c150
Otlotleng24 <phenyomodise.pm@gmail.com>
2023-07-19 12:32:27 -0700

    4-isalpha

--- 0x02-functions_nested_loops/4-main.c
+++ 0x02-functions_nested_loops/4-main.c
@@ -2,20 +2,20 @@
 
 /**
  * main - check the code.
- *
  * Return: Always 0.
  */
+
 int main(void)
 {
     int r;
 
-    r = isalpha('H');
+    r = _isalpha('H');
     _putchar(r + '0');
-    r = isalpha('o');
+    r = _isalpha('o');
     _putchar(r + '0');
-    r = isalpha(108);
+    r = _isalpha(108);
     _putchar(r + '0');
-    r = isalpha(';');
+    r = _isalpha(';');
     _putchar(r + '0');
     _putchar('\n');
     return (0);
