ad1a59d8bf6685ecb593ed3fc9c14bcb5ce7766e
Otlotleng24 <phenyomodise.pm@gmail.com>
2023-07-19 15:25:35 -0700

    10-

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - Adds two numbers
+ * @n1: First number to be computed
+ * @n2: Second number to be computed
+ * Retuen: Integer value
+ */
+
+int add(int n1, int n2)
+
+{
+
+	return (n1 + n2);
+}
