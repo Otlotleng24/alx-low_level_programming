4b6e68964ddb0ab01eda8093018b0be6534354ba
Otlotleng24 <phenyomodise.pm@gmail.com>
2023-07-20 11:58:05 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * _isuper - Checked if a letter is upper
- * @x: The nmber to be checked
- * Return: 1 for the uppper letter or 0 for any else
+ * _isupper - Check if a letter is upper
+ * @x: The number to be checked
+ * Return: 1 for the uppper letter or 0 for anything else
  */
 
 int _isupper(int c)
